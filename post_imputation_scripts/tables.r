defd2f7e119be7af99f63621f43b273f85a74d20
filippob <filippo.biscarini@gmail.com>
2025-12-08 20:53:59 +0100

    added: first stub of script to make tables for the article

---
+++ post_imputation_scripts/tables.r
@@ -0,0 +1,159 @@
+
+## R script to make tables for the article
+
+library("dplyr")
+library("data.table")
+
+base_folder = '~/Documents/chiara/imputation/Analysis'
+experiment = 'gap'
+# dataset = 'line1_filtered'
+outdir = 'results/tables'
+
+#################
+## GAP IMPUTATION
+#################
+
+## avg kappa
+df = data.frame(NULL)
+species_list = c("goat","cattle","peach","maize","simdata")
+
+for (species in species_list) {
+  
+  print(species)
+  fname = paste("summary_", experiment,".csv", sep="")
+  list_of_files <- list.files(path = file.path(base_folder, species, "results"),
+                              recursive = TRUE,
+                              pattern = fname,
+                              full.names = TRUE)
+  
+  print(paste("reading", length(list_of_files), "files from folder", species, "/", "results"))
+  
+  temp <- list_of_files %>%
+    set_names() %>% 
+    map_df(read_csv, .id = "file_name", show_col_types = FALSE) 
+  
+  temp <- select(temp, -any_of(c("78"))) 
+  
+  temp$species = species
+  temp$experiment_type = experiment
+  
+  df <- rbind.data.frame(df,temp)
+}
+
+df <- df |>
+  mutate(experiment_name = gsub("_filtered", "", experiment_name)) |>
+  rename(dataset = experiment_name)
+
+dir.create(file.path(base_folder, outdir), showWarnings = FALSE)
+fname = paste(experiment, "kappa", "avg.csv", sep="_")
+
+fwrite(df, file = file.path(base_folder, outdir, fname))
+
+## standard deviation of kappa
+df = data.frame(NULL)
+species_list = c("goat","cattle","peach","maize","simdata")
+
+for (species in species_list) {
+  
+  print(species)
+  fname = paste("std_kappa_", experiment,".csv", sep="")
+  list_of_files <- list.files(path = file.path(base_folder, species, "results"),
+                              recursive = TRUE,
+                              pattern = fname,
+                              full.names = TRUE)
+  
+  print(paste("reading", length(list_of_files), "files from folder", species, "/", "results"))
+  
+  temp <- list_of_files %>%
+    set_names() %>% 
+    map_df(read_csv, .id = "file_name", show_col_types = FALSE) 
+  
+  temp <- select(temp, -any_of(c("78"))) 
+  
+  temp$species = species
+  temp$experiment_type = experiment
+  
+  df <- rbind.data.frame(df,temp)
+}
+
+df <- df |>
+  mutate(experiment_name = gsub("_filtered", "", experiment_name)) |>
+  rename(dataset = experiment_name, miss_rate = proportion_missing, experiment = experiment_type)
+
+dir.create(file.path(base_folder, outdir), showWarnings = FALSE)
+fname = paste(experiment, "std", "kappa.csv", sep="_")
+
+fwrite(df, file = file.path(base_folder, outdir, fname))
+
+## experimental plan
+df = data.frame(NULL)
+species_list = c("goat","cattle","peach","maize","simdata")
+
+for (species in species_list) {
+  
+  print(species)
+  fname = paste("exp_plan_", experiment,".csv", sep="")
+  list_of_files <- list.files(path = file.path(base_folder, species, "results"),
+                              recursive = TRUE,
+                              pattern = fname,
+                              full.names = TRUE)
+  
+  print(paste("reading", length(list_of_files), "files from folder", species, "/", "results"))
+  
+  temp <- list_of_files %>%
+    set_names() %>% 
+    map_df(read_csv, .id = "file_name", show_col_types = FALSE) 
+  
+  temp <- select(temp, -any_of(c("78"))) 
+  
+  temp$species = species
+  temp$experiment_type = experiment
+  
+  df <- rbind.data.frame(df,temp)
+}
+
+df <- df |>
+  mutate(experiment_name = gsub("_filtered", "", experiment_name)) |>
+  rename(dataset = experiment_name, miss_rate = proportion_missing, experiment = experiment_type)
+
+dir.create(file.path(base_folder, outdir), showWarnings = FALSE)
+fname = paste(experiment, "experiment", "plan.csv", sep="_")
+
+fwrite(df, file = file.path(base_folder, outdir, fname))
+
+## number of missing genotypes (size of the problem)
+df = data.frame(NULL)
+species_list = c("goat","cattle","peach","maize","simdata")
+
+for (species in species_list) {
+  
+  print(species)
+  fname = paste("num_missing_", experiment,".csv", sep="")
+  list_of_files <- list.files(path = file.path(base_folder, species, "results"),
+                              recursive = TRUE,
+                              pattern = fname,
+                              full.names = TRUE)
+  
+  print(paste("reading", length(list_of_files), "files from folder", species, "/", "results"))
+  
+  temp <- list_of_files %>%
+    set_names() %>% 
+    map_df(read_csv, .id = "file_name", show_col_types = FALSE) 
+  
+  temp <- select(temp, -any_of(c("78"))) 
+  
+  temp$species = species
+  temp$experiment_type = experiment
+  
+  df <- rbind.data.frame(df,temp)
+}
+
+df <- df |>
+  mutate(experiment_name = gsub("_filtered", "", experiment_name)) |>
+  rename(dataset = experiment_name, miss_rate = proportion_missing, experiment = experiment_type)
+
+dir.create(file.path(base_folder, outdir), showWarnings = FALSE)
+fname = paste(experiment, "num", "missing.csv", sep="_")
+
+fwrite(df, file = file.path(base_folder, outdir, fname))
+
