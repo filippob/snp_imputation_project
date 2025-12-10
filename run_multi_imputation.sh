33f412b74fc737e1537f650ce30163c28dc1bc48
filippob <filippo.biscarini@gmail.com>
2025-12-10 17:11:29 +0100

    preparing section to run density imputation experiments

--- run_multi_imputation.sh
+++ run_multi_imputation.sh
@@ -12,17 +12,18 @@
 ##########################################################
 ## PARAMETERS
 ##########################################################
-datafolder="data/sheep/filtered_data"
+datafolder="data/goat/filtered_data"
 pops="nss ts"
 target="ts"
-sample_size=60
-missrate=0.1 ## for gap and mixed imputation experiments
+sample_size=70
+ld_size=20
+missrate=0.05 ## for gap and mixed imputation experiments
 relationship="distant" ## for across-population experiments
-dataset="AustralianSuffolk_filtered" ## peach: combined_18k_filtered; goat: goat_filtered etc.
-species="sheep"
+dataset="BOE_filtered" ## peach: combined_18k_filtered; goat: goat_filtered etc.
+species="goat" ## this commands the output folder
 ld_array=""
 #ld_array="/home/freeclimb/data/peach/SNP_array/snp_names.9k"
-exp_type="GAP" ## ACROSS, DENSITY, GAP
+exp_type="DENSITY" ## ACROSS, DENSITY, GAP
 #########################################################
 
 n=3 ## n. of replicates to run
@@ -57,6 +58,11 @@ then
 	#for dataset in nss_filtered;
 	#for dataset in CxEL_filtered DxP_filtered pop001_filtered pop004_filtered;
 	#for dataset in ALP_filtered ANG_filtered BRK_filtered BOE_filtered CRE_filtered LNR_filtered;
+	for i in $(seq 1 $n);
+	do
+		echo "running replicate $i on dataset $dataset"
+		bash snp_imputation_project/run_densityimputation.sh $datafolder/$dataset ${sample_size} ${ld_size} $species
+	done
 fi
 
 ## ACROSS IMP
