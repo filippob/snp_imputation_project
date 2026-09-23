bc3c87fbb4ff50b81f2acd993f397f6c9b39c173
filippob <filippo.biscarini@gmail.com>
2026-09-23 15:42:32 +0200

    modified: repo for QMSim software specified as parameter

--- simulation_scripts/1.simulate_genotypes.sh
+++ simulation_scripts/1.simulate_genotypes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 prjfolder="$HOME/Documents/chiara/imputation/simulation"
-
+repo="$HOME/Documents/chiara/imputation/snp_imputation_project/simulation_scripts"
 cd $prjfolder
 
 echo " - running simulations ... "
-./simulation_software/QMSim2 paramfiles/sim1.prm -o
+$repo/simulation_software/QMSim2 paramfiles/sim1.prm -o
 
 echo "DONE!"
