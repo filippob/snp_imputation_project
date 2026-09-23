01b52731c3d9df0a73a4af559b7897459f0b78c1
filippob <filippo.biscarini@gmail.com>
2026-09-23 15:01:53 +0200

    commented script

--- simulation_scripts/1.2.preprocess_simdata.sh
+++ simulation_scripts/1.2.preprocess_simdata.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+## script that takes the simulate map file and multiplies the SNP positions
+## to make them integers and not floats
+
 prjfolder="$HOME/Documents/chiara/imputation/simulation"
 expfolder="r_sim1"
 mapfile="lm_mrk_001.txt"
 
 cd $prjfolder
 
-echo " - preprocessing simulated data (multiplying marker position by power of 10)"
+echo " - preprocessing simulated data (multiplying marker position by power of 10: 10^5)"
 tail -n +2 ${expfolder}/$mapfile > temp
 awk '{print $3*100000}' temp > pos
 awk '{print $1 "   " $2}' temp > tmp
